3af6f597f52420cf540642bdf06d53a75112d33f
Harman <harmany@hotmail.ca>
2023-06-08 18:06:00 -0400

    simple layout created, now I should work on fetching the data and displaying it in a recycler view

--- app/src/main/java/com/example/workday_nasamobileapp/MainActivity.java
+++ app/src/main/java/com/example/workday_nasamobileapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.workday_nasamobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button search_btn, p_page_btn, n_page_btn;
+    EditText search_query;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+
     }
 }
